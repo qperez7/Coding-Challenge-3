f075c06e3a8bb7761a8ceca307f7061d604e15f9
Quirianyailet Perez <qperez1@usf.edu>
2024-09-08 12:07:20 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,10 +1,39 @@
 //Task1: Create a Function to Calculate Average Sales
-let salesperson = { name: 'Alice', sales: [12000, 15000, 13000]};
-console.log(salesperson,sales);
-let salespersonSum = salesperson.reduce((sales,saleperson) => sales,salesperson);
-console.log(`Sum of SalesPerson Alice: $${sales}`);
-function calculateAverageSales (salespersonSum,quantity) {
-    return salespersonSum / 3;
+const salesData = [
+    { name: 'Alice', sales: [12000, 15000, 13000] },
+    { name: 'Bob', sales: [7000, 6000, 7500] },
+    { name: 'Charlie', sales: [3000, 4000, 3500] },
+    { name: 'Diana', sales: [9000, 8500, 9200] },
+    ];
+    function calculateAverageSales(name) {
+        let sum = name.sales.reduce((accumulator, currentValue) => accumulator + currentValue,0);
+    return sum / name.sales.length;
+    }
+    salesData.forEach(name => {
+        name.sales =calculateAverageSales(name);
+        });
+    salesData.sort((a,b) => a.sales - b.sales);
+    console.log(salesData); // Output average sales for each sales person in ascending order
+
+    // Task2 : Create a Function to Determine Performance Rating
+    function determineperformanceRating(name) {
+        let rates =name.sales;
+        
+    if (rates > 10000) {
+        console.log (`“Excellent” for average sales above $10,000`);
+    } else if (rates >= 7000) {
+        console.log (`“Good” for average sales between $7,000 and $10,000`);
+    } else if (rates >=4000) {
+        console.log (`“Satisfactory” for average sales between $4,000 and $7,000`);
+    } else {
+        console.log (`“Needs Improvement” for average sales below $4,000`);
+    }
+    return rates;
+    
 }
-console.log(`Alices Average Sales: ${calculateAverageSales}`);
+salesData.forEach(name => {
+    determineperformanceRating(name);
+    });
+    salesData.sort((a,b) => a.sales - b.sales); // Output returns rates based on ascending averages
+
 
